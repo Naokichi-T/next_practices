1d0fbc25c2db1f6b9dc660a0249f5f98094868d3
Naokichi-T <naoki.tany@gmail.com>
2021-01-02 17:42:20 +0900

    5-27

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import Counter from "../components/Counter";
 
 export default () => (
   <Layout header="Next" title="Top page.">
-    <p>Welcome to next.js!</p>
+    <Counter />
     <hr />
     <Link href="./other">
-      <button>Go to Other &gt;&gt;</button>
+      <button>go to Other &gt;&gt;</button>
     </Link>
   </Layout>
 );
