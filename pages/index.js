43b48d22e2c4baa05cb436ef98109f2067945efa
Naokichi-T <naoki.tany@gmail.com>
2021-01-02 15:27:34 +0900

    5-18

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Counter from "../components/Counter";
-import style from "../static/Style";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 export default () => (
-  <div>
-    {style}
-    <h1>Next.js</h1>
+  <Layout header="Next" title="Top page.">
     <p>Welcome to next.js!</p>
     <hr />
-    <Counter />
-  </div>
+    <Link href="./other">
+      <button>Go to Other &gt;&gt;</button>
+    </Link>
+  </Layout>
 );
